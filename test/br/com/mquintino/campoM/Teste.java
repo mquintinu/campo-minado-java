242f3c872b1c87f15f29be8cd7478923e2608b58
Mateus Quintino <mateusq1@hotmail.com>
2022-01-28 01:03:47 -0300

    Subindo projeto na estrutura do Eclipse

---
+++ test/br/com/mquintino/campoM/Teste.java
@@ -0,0 +1,17 @@
+package br.com.mquintino.campoM;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Teste {
+
+	@Test
+	void testarSeIgualA2() {
+		int a = 1 + 1;
+		
+		assertEquals(2, a); 
+	}
+
+	
+}
